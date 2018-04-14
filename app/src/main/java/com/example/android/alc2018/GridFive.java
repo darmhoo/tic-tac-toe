40cce256d070968eec7c08978b7150ee20acf2ed
Omodamola <darmhoo@hotmail.com>
2018-04-14 19:08:49 +0100

    first commit

--- app/src/main/java/com/example/android/alc2018/GridFive.java
+++ app/src/main/java/com/example/android/alc2018/GridFive.java
@@ -2,12 +2,34 @@ package com.example.android.alc2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class GridFive extends AppCompatActivity {
+public class GridFive extends AppCompatActivity implements View.OnClickListener{
+
+    private final static String TAG = GridFive.class.getSimpleName();
+    boolean PLAYER_X = true;
+
+    int TURN_COUNT = 0;
+    Button b00;
+    Button b01;
+    Button b02;
+    Button b03;
+    Button b04;
+
+    Button b10;
+    Button b11;
+    Button b12;
+    Button b13;
+    Button b14;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_five);
+    }
+    public void onClick(View view){
+
     }
 }
