22bda74580f02a6ffb55ec0bf5417ec0213238fd
Omodamola <darmhoo@hotmail.com>
2018-04-14 16:14:50 +0100

    first commit

---
+++ app/src/main/java/com/example/android/alc2018/GridFive.java
@@ -0,0 +1,13 @@
+package com.example.android.alc2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridFive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_five);
+    }
+}
